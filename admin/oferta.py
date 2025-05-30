e7b24b4b7cfa7d4e6df9a6f52b782d0a0c96bbce
Valen Bustamante <117618602+valenbustamante@users.noreply.github.com>
2025-05-30 15:19:25 -0500

    s

--- admin/oferta.py
+++ admin/oferta.py
@@ -3,7 +3,7 @@ import psycopg2
 from datetime import datetime
 import os
 from dotenv import load_dotenv
-from auth_utils import get_connection
+from utils import get_connection
 load_dotenv()
 
 st.title('Gestión de oferta académica')
